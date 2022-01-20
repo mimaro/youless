74cad64de0b41a34079fae8dc4a22f9994106b49
mimaro <46071857+mimaro@users.noreply.github.com>
2022-01-20 22:23:54 +0100

    Update youless.py

--- youless.py
+++ youless.py
@@ -6,31 +6,23 @@ import requests
 import json
 import logging
 
-# Impulsrate definieren
+
+IP_YOULESS = "http://192.168.178.31/a?f=j"
+IMPULSRATE = 10000
 
 def main():
 
     impuls = int(open("impuls.txt").read())
 
     # Leistungswert von youless auslesen
-    req = requests.get("http://169.254.32.1/a?f=j")
+    req = requests.get("IP_YOULESS)
     data = req.content
     decoded_data = json.loads(data)
     power = str(("{}".format(decoded_data["pwr"])))
-    power_corr = str(float(power) / impuls * 1000)
+    power_corr = str(float(power) / impuls * IMPULSRATE)
 
     print(power_corr)
     print(impuls)
-
-    # Werte in txt File schreiben
-    data_out = []
-    data_out = open("daten.txt", "a")
-    data_out.write(power_corr + "\n")
-    data_out.close()
-
-    power = open("power.txt", "w")
-    power.write(power_corr)
-    power.close()
 
 if __name__ == "__main__":
     main()

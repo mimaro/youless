13f438e602ddd8e0ddb6eb967083b7e2427932f9
mimaro <46071857+mimaro@users.noreply.github.com>
2022-01-21 13:17:27 +0100

    Update youless.py

--- youless.py
+++ youless.py
@@ -4,28 +4,44 @@
 import requests
 import json
 import logging
+import time
 
+VZ_POST_URL = "http://vz.wiuhelmtell.ch/middleware.php/data/{}json?operation=add&value={}"
+UUID = {
+    "Power": "ad5c8090-3698-11ea-8ad7-7f796afef9a1"
+    {
+IP_YOULESS = "http://156.154.187.10/a?f=j"
+IMPULSRATE = 1000
 
-IP_YOULESS = "http://192.168.178.31/a?f=j"
-IMPULSRATE = 10000
+def write_vals(uuid,val):
+    poststring = VZ_POST_URL.format(uuid,val)
+    postreq = requests.post(poststring)
 
 def main():
-    # W-LAN deaktivieren
-    sudo ifconfig eth0 down
+   avg_pwr = 0
     
-    # Leistungswert von youless auslesen
+   for i in range (5): 
+        # Leistungswert 1-5 von youless auslesen
+        req = requests.get(IP_YOULESS)
+        data = req.content
+        decoded_data = json.loads(data)
+        power = str(("{}".format(decoded_data["pwr"])))
+        power_corr = str(float(power) / impuls * IMPULSRATE)
+        avg_pwr += power_corr
+        print(power_corr)
+        time.sleep(10)
+
     req = requests.get(IP_YOULESS)
     data = req.content
     decoded_data = json.loads(data)
     power = str(("{}".format(decoded_data["pwr"])))
     power_corr = str(float(power) / impuls * IMPULSRATE)
-
+    avg_pwr += power_corr
     print(power_corr)
-    print(impuls)
-
-    #W-LAN einschalten
-    sudo ifconfig eth0 up
     
+    avg_pwr = avg_pwr/6
+    write_vals(UUID["Power"],avg_pwr)
+    print(avg_pwr)
     
 if __name__ == "__main__":
     main()
